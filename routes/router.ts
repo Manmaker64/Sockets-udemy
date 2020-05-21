4477fc20bafd350fab6f69b4556f394a9ce9942b
Manmaker64 <joseramon64@hotmail.com>
2020-05-21 17:56:36 +0200

    Sección 6 lista

--- routes/router.ts
+++ routes/router.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import Server from '../classes/server';
+import { usuariosConectados } from '../sockets/socket';
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.get('/mensajes', ( req: Request, res: Response ) => {
     });
 });
 
+// Servicio para eviar mensajes a todos los usuarios
 router.post('/mensajes', ( req: Request, res: Response ) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
@@ -25,6 +27,7 @@ router.post('/mensajes', ( req: Request, res: Response ) => {
     });
 });
 
+// Servicio para enviar mensajes privados a un único usuario (ID)
 router.post('/mensajes/:id', ( req: Request, res: Response ) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
@@ -39,6 +42,34 @@ router.post('/mensajes/:id', ( req: Request, res: Response ) => {
         cuerpo,
         de,
         id
+    });
+});
+
+// Servicio para obtener todos los IDs de los usuarios
+router.get('/usuarios', ( req: Request, res: Response ) => {
+    const server = Server.instance;
+
+    server.io.clients( (err: any, clientes: string[] ) => {
+        if ( err ) {
+            return res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                clientes
+            });
+        }
+    });
+});
+
+// Obtener usuarios y sus nombres
+router.get('/usuarios/detalle', ( req: Request, res: Response ) => {
+    
+    res.json({
+        ok: true,
+        clientes: usuariosConectados.getLista()
     });
 });
 
